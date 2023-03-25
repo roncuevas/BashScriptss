b50efb4a74a32e71e592b02ff08c5c30a05504b2
Aaron Cuevas <contalado0r@gmail.com>
2023-03-25 17:21:16 -0600

    Change dependencies

--- vaporInstaller.sh
+++ vaporInstaller.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo "Installing Vapor dependencies..."
-sudo apt-get install binutils git gnupg2 libc6-dev libcurl4-openssl-dev 
-     \ libedit2 libgcc-9-dev libpython3.8 libsqlite3-0 libstdc++-9-dev 
-     \ libxml2-dev libz3-dev pkg-config tzdata unzip zlib1g-dev
+sudo apt-get install binutils git gnupg2 libc6-dev libcurl4-openssl-dev libedit2 libgcc-9-dev libpython3.8 libsqlite3-0 libstdc++-9-dev libxml2-dev libz3-dev pkg-config tzdata unzip zlib1g-dev
 
 echo "Cloning Vapor..."
 git clone https://github.com/vapor/toolbox.git
